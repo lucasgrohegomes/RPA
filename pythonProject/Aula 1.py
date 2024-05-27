6fd4518a9a5fc5f5f396789e9030e129904e2f25
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-05-27 17:13:21 -0300

    commit2

--- pythonProject/Aula 1.py
+++ pythonProject/Aula 1.py
@@ -40,33 +40,44 @@ import pygetwindow as gw
 
 option = p.confirm('Olá, por favor, escolha o programa desejado:', buttons=['EXCEL', 'WORD', 'NOTEPAD'])
 
+r.init(chrome_browser=False, visual_automation=True)
 if option == 'EXCEL':
     print('Você escolheu o Excel.')
     os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk')
-    p.sleep(7.5)
-    janela = gw.getWindowsWithTitle('Excel')
-    janela.maximize()
-
-    r.init(chrome_browser=False, visual_automation=True)
-
+    p.sleep(3)
     if r.present('excelAberto.png'):
-
-        p.hotkey('enter')
-
+        print('Abriu!')
+        r.click('excelAberto.png')
     else:
         r.wait(r.present('excelAberto.png'))
-        print('Aguardando Excel.')
-    r.close()
+        print('Esperando Excel...')
+        r.click('excelAberto.png')
 
 elif option == 'WORD':
     print('Você escolheu o Word.')
     os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk')
-    p.sleep(7.5)
-    janela = gw.getWindowsWithTitle('Word')
+    p.sleep(4)
+    janela = gw.getActiveWindow()
     janela.maximize()
+    if r.present('wordAberto.png'):
+        print('Abriu!')
+        r.click('wordAberto.png')
+    else:
+        r.wait(r.present('wordAberto.png'))
+        print('Esperando Word...')
+        r.click('wordAberto.png')
 
 elif option == 'NOTEPAD':
-    print('Você escolheu o NotePad')
+    print('Você escolheu o NotePad.')
     os.startfile(r'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Accessories\Notepad.lnk')
-    p.sleep(7.5)
-    
+    p.sleep(4)
+    janela = gw.getActiveWindow()
+    janela.maximize()
+    if r.present('notepadAberto.png'):
+        print('Abriu!')
+        r.click('notepadAberto.png')
+    else:
+        r.wait(r.present('notepadAberto.png'))
+        print('Esperando NotePad...')
+        r.click('notepadAberto.png')
+r.close()

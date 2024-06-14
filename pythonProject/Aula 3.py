f7a18141391269c3b7b23181b9f1fc4af6fdaa97
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-06-14 17:31:52 -0300

    socorororo

--- pythonProject/Aula 3.py
+++ pythonProject/Aula 3.py
@@ -1,14 +1,28 @@
+import pyperclip
 import rpa as r
 import pyautogui as p
 import pandas as pd
 import os
 import pygetwindow as pw
+import selenium
+import pyperclip as pc
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
 
 r.init(visual_automation=True, chrome_browser=True)
 
-r.url('https://br.advfn.com/monitor/')
-p.sleep(4)
+r.url('https://br.advfn.com/p.php?pid=front&logout=1')
+p.sleep(20)
 janela = pw.getActiveWindow()
 janela.maximize()
+p.sleep(3)
+r.click('/html/body/div/div[2]/div[2]/div/form/div[1]/input')
 p.sleep(2)
-# r.click('xpath=//*[@id="afnmainbodid"]/div[1]/div/div[5]/a[1]')
+p.hotkey('ctrl','a')
+p.sleep(1)
+p.hotkey('backspace')
+p.sleep(20)
+r.type('//*[@id="afnmainbodid"]/div/div[2]/div[2]/div/form/div[1]/input', 'lucasgomes2')
+r.type('//*[@id="password-input"]', 'Lucasgg10*[enter]')
+p.sleep(3)
+r.close()

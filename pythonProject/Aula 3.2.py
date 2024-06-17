084e8a80106d9e55449ee6468fc20ba8f03b7b5b
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-06-17 17:22:49 -0300

    ultimaaulagg

--- pythonProject/Aula 3.2.py
+++
@@ -1,4 +0,0 @@
-import pyautogui as p
-
-p.sleep(5)
-print(p.position())
---
+++ pythonProject/Aula 4.py
@@ -0,0 +1,9 @@
+import pyautogui as p
+import pandas as pd
+from selenium.webdriver import Chrome
+import warnings
+
+# IMPORTANDO OS DADOS
+
+with warnings.catch_warnings(record=True):
+    warnings
